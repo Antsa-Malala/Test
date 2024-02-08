2dca62b4b89ad67c7f598611823f5055d280fa2a
Antsa-Malala <119752846+Antsa-Malala@users.noreply.github.com>
2024-02-09 00:14:37 +0300

    Update

--- src/main/java/org/project/clouds5_backend/config/FirebaseConfig.java
+++ src/main/java/org/project/clouds5_backend/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package org.project.clouds5_backend.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -11,18 +12,22 @@ import org.springframework.core.io.Resource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
+
+
+    @Value("${app.firebase-configuration-file}")
+    private String firebaseConfigPath;
     @Bean
     public FirebaseMessaging firebaseMessaging() throws IOException {
-        Resource resource = new ClassPathResource("privateKey/adminsdk.json");
-        FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+        Resource resource = new ClassPathResource(firebaseConfigPath);
+        InputStream serviceAccount = new ClassPathResource(firebaseConfigPath).getInputStream();
 
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://<DATABASE_NAME>.firebaseio.com/")
                     .build();
 
             FirebaseApp.initializeApp(options);
